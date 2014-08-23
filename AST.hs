7277a7a85fa4a524f0b80fbae595e1f17a89804b
ryna4c2e <ryna4c2e@gmail.com>
2014-08-24 00:54:36 +0900

    wrote some comments on AST.hs

--- AST.hs
+++ AST.hs
@@ -1,16 +1,24 @@
+-- AST（Abstract Syntax Tree）を定義する．
+-- データ型の定義以外の役割を持たない．
+
+
 module AST where
 
+-- 32bitのデータを扱うため
 import Data.Word
 
 
-
+-- ポインタ変数を表すノード
 data PtrVar = PtrVar String deriving (Show, Eq)
+
+-- こちらは32bitの整数を表すノード
 data IntVar = IntVar String deriving (Show, Eq)
 
+-- 整数の値を利用している箇所を表すノード
 data IntValue = Const Word32 
               | GetVar IntVar deriving (Show, Eq)
 
-
+-- 簡単な式．
 data SimpleExpr = Add IntValue IntValue
                 | Mul IntValue IntValue
 
@@ -28,8 +36,10 @@ data SimpleExpr = Add IntValue IntValue
                 | Expr  IntValue
                   deriving (Show, Eq)
 
+-- とりあえず，ASTは文の連続とする．
 data AST = AST [Sentence] deriving (Show, Eq)
 
+-- 文．
 data Sentence = Assign IntVar SimpleExpr
               | If SimpleExpr AST AST
               | While SimpleExpr AST
